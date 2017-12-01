3fd4715c709db5320df046c084928aea3ccc36a7
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-11-30 23:43:24 -0600

    finished cpt telemaraton

--- archive-telemaraton2016.php
+++ archive-telemaraton2016.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
   Template Name: Categorias
@@ -25,22 +25,22 @@
                   <a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                   <div class="date-cat">
                     <div class="row">
-                        <div class="col-xs-12 col-sm-6 col-md-6 fecha">                        
+                        <div class="col-xs-12 col-sm-6 col-md-6 fecha">
                             <span class="label1"><i class="icon-date fa fa-calendar"></i><?php the_time('j  '); ?></span>
                             <span class="label2"><?php the_time('F '); ?></span>
                           </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 descripcion_categoria"><i class="icon-file fa fa-file"></i><a class="cat"><?php the_category (' , '); ?></a></div>
+                        <!--div class="col-xs-12 col-sm-6 col-md-6 descripcion_categoria"><i class="icon-file fa fa-file"></i><a class="cat"><?php // post_type_archive_title(); ?></a></div -->
                     </div>
                   </div>
                   <div class="thumb">
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>              
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>
                   </div>
-                  <div class="info"><?php the_excerpt();  ?></div>
-            </div>   
+                  <!-- div class="info"><?php // the_excerpt();  ?></div -->
+            </div>
 
 
           <?php endwhile; ?>
-          <div class="navigation"><?php if(function_exists('pagenavi')) { pagenavi(); } ?></div>
+          <!-- div class="navigation"><?php // if(function_exists('pagenavi')) { pagenavi(); } ?></div -->
           <?php else : ?>
           <p><?php _e('Ups!, no hay entradas.'); ?></p>
           <?php endif; ?>
@@ -51,14 +51,14 @@
             <div class="recientes">
               <?php get_sidebar(); ?>
               <div class="facebook">
-                <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FTvArquidiocesana&width=292&height=590&colorscheme=light&show_faces=true&header=true&stream=true&show_border=true&appId=165911470135594" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:590px;" allowTransparency="true"></iframe>                    
+                <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FTvArquidiocesana&width=292&height=590&colorscheme=light&show_faces=true&header=true&stream=true&show_border=true&appId=165911470135594" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:590px;" allowTransparency="true"></iframe>
               </div>
-            </div>  
-          </div> 
-      </div>    
-    </div>      
+            </div>
+          </div>
+      </div>
     </div>
-    
+    </div>
+
 </section>
 
 
--- single-telemaraton2016.php
+++ single-telemaraton2016.php
@@ -1,6 +1,6 @@
 <!-- Archivo de cabecera gobal de Wordpress -->
 
-<?php 
+<?php
 
 /*
   Template Name: sigle
@@ -23,23 +23,23 @@
             <div class="contenido">
               <div class="date-cat">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-6 col-md-6 fecha">                        
+                    <div class="col-xs-12 col-sm-6 col-md-6 fecha">
                         <span class="label1"><i class="icon-date fa fa-calendar"></i><?php the_time('j  '); ?></span>
                         <span class="label2"><?php the_time('F '); ?></span>
                       </div>
-                    <div class="col-xs-12 col-sm-6 col-md-6 descripcion_categoria"><i class="icon-file fa fa-file"></i><a class="cat"><?php the_category (' , '); ?></a></div>
+                    <!-- div class="col-xs-12 col-sm-6 col-md-6 descripcion_categoria"><i class="icon-file fa fa-file"></i><a class="cat"><?php // the_category (' , '); ?></a></div -->
                 </div>
-              </div>              
+              </div>
               <div class="info">
             <!-- h1><?php //   the_title(); ?></h1 -->
              <?php
               while ( have_posts() ) : the_post();
             //          get_template_part( 'content', 'page' );
-              the_content();           
+              the_content();
               endwhile;
               ?>
               </div>
-            </div> 
+            </div>
        </div>
       <div class="col-xs-12 col-sm-12 col-md-4 side">
           <div class="entradas">
@@ -47,14 +47,14 @@
             <div class="recientes">
               <?php get_sidebar(); ?>
               <div class="facebook">
-                <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FTvArquidiocesana&width=292&height=590&colorscheme=light&show_faces=true&header=true&stream=true&show_border=true&appId=165911470135594" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:590px;" allowTransparency="true"></iframe>                    
+                <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FTvArquidiocesana&width=292&height=590&colorscheme=light&show_faces=true&header=true&stream=true&show_border=true&appId=165911470135594" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:590px;" allowTransparency="true"></iframe>
               </div>
-            </div>  
-          </div> 
-      </div>    
+            </div>
+          </div>
+      </div>
     </div>
   </div>
-  
+
 </section>
 
 <?php get_footer(); ?>
