53403ec8cd9041d28f3ac08eaae8b85594c214c9
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-11-30 18:00:13 -0600

    60% caminantes

--- archive-telemaraton2017.php
+++ archive-telemaraton2017.php
@@ -13,37 +13,28 @@
 
   <div class="container">
      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-8 con">
+        <div class="col-md-12 con">
             <?php // echo 'estoy en category'; ?>
           <div class="titulo">
             <div class="mapeo"><?php the_breadcrumb(); ?></div>
           </div>
+    <div class="cuadro-cpts">
             <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
-
            <div class="contenido">
                   <a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  <div class="date-cat">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6 col-md-6 fecha">
-                            <span class="label1"><i class="icon-date fa fa-calendar"></i><?php the_time('j  '); ?></span>
-                            <span class="label2"><?php the_time('F '); ?></span>
-                          </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 descripcion_categoria"><i class="icon-file fa fa-file"></i><a class="cat"><?php the_category (' , '); ?></a></div>
-                    </div>
-                  </div>
                   <div class="thumb">
                     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>
                   </div>
                   <!-- div class="info"><?php // the_excerpt();  ?></div -->
             </div>
 
-
           <?php endwhile; ?>
           <div class="navigation"><?php if(function_exists('pagenavi')) { pagenavi(); } ?></div>
           <?php else : ?>
           <p><?php _e('Ups!, no hay entradas.'); ?></p>
           <?php endif; ?>
+        </div><!-- fin de cuadro CPTS -->
        </div>
     </div><!-- fin del RoW -->
     </div>
