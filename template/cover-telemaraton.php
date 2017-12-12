fd07a7490f4af21ef78415497ae0b625c91fd723
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-12-12 14:46:48 -0600

    jtv maraton 2017

--- template/cover-telemaraton.php
+++ template/cover-telemaraton.php
@@ -10,9 +10,9 @@
 Este 1, 2 y 3 de diciembre, parte de nuestro público, ven a Rectoría de Santa a la 5ta. calle 3-61 zona 1.
 Para que Jesucristo sea más conocido, amado y seguido.
    </p>
-   <a href="<?php echo esc_url( home_url( '/' ) ); ?>/telemaraton2017" class="btn-2017">INGRESAR A TELEMARATON 2017</a>
+   <a href="<?php echo esc_url( home_url( '/' ) ); ?>telemaraton2017" class="btn-2017">INGRESAR A TELEMARATON 2017</a>
    <div class="resumen16">
      <p>Resumen de nuestra primera Telemaratón 2,016</p>
    </div>
-   <a href="<?php echo esc_url( home_url( '/' ) ); ?>/telemaraton2016" class="btn-2016">INGRESAR A TELEMARATON 2016</a>
+   <a href="<?php echo esc_url( home_url( '/' ) ); ?>telemaraton2016" class="btn-2016">INGRESAR A TELEMARATON 2016</a>
  </div>
